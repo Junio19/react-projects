335b99208ce7034e2a252f25517c098e2a206072
Junio19 <johnpatrickbruzovalencia@gmail.com>
2023-01-10 18:29:05 +0800

    Initial blog design

---
+++ src/Home.js
@@ -0,0 +1,31 @@
+import { useState, useEffect } from "react";
+import BlogList from "./BlogList";
+
+const Home = () => {
+    const[blogs,setBlogs] = useState(null)
+
+    const[name,setName] = useState('mario')
+
+    const handleDelete = (id) => {
+        const newBlogs = blogs.filter(blog => blog.id !== id )
+        setBlogs(newBlogs)
+    }
+
+    useEffect(()=>{
+        fetch('http://localhost:8000/blogs')
+            .then(res => {
+                return res.json()
+            })
+            .then(data => {
+                setBlogs(data)
+            })
+    },[])
+    
+    return ( 
+        <div className="home">
+           {blogs && <BlogList blogs={blogs} title='All blogs' handleDelete={handleDelete}/>}
+        </div>
+     );
+}
+ 
+export default Home;
---
+++ src/Navbar.js
@@ -0,0 +1,14 @@
+const Navbar = () => {
+    return (
+        <div className="navbar">
+            <h2>The Dojo Blog</h2>
+            <div className="links">
+                <a href="/">Home</a>
+                <a href="/create">New Blog</a>
+            </div>
+        </div>  
+        
+    );
+}
+ 
+export default Navbar;
